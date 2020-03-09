5b72da40a28b1817477a958d84434dc4fb86c613
Ashwin Ganesh <ashytech10@gmail.com>
2020-03-09 16:51:57 -0400

    made new telescope, now uses joystick

--- src/main/java/frc/robot/commands/climb_commands/MoveTelescope.java
+++ src/main/java/frc/robot/commands/climb_commands/MoveTelescope.java
@@ -7,35 +7,44 @@
 
 package frc.robot.commands.climb_commands;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Climb;
 
-public class StartTelescopeMotors extends CommandBase {
-  private final Climb m_Climb;
+public class MoveTelescope extends CommandBase {
+  private Climb m_climb;
+  private RobotContainer m_container;
+
   /**
-   * Creates a new StartTelescopeMotors.
+   * Creates a new MoveTelescope.
    */
-  public StartTelescopeMotors(Climb climb) {
+  public MoveTelescope(Climb subsystem) {
+    m_climb = subsystem;
+    addRequirements(m_climb);
     // Use addRequirements() here to declare subsystem dependencies.
-    m_Climb = climb;
-    addRequirements(m_Climb);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_Climb.SetTelescopeMotors(0.5);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    double leftY = m_container.operator.getY(Hand.kLeft);
+    double rightY = m_container.operator.getY(Hand.kRight);
+    m_climb.SetWinchMotors(-leftY);
+    m_climb.SetTelescopeMotor(rightY);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_Climb.SetTelescopeMotors(0);
+    m_climb.SetTelescopeMotor(0);
+    m_climb.SetWinchMotors(0);
   }
 
   // Returns true when the command should end.
